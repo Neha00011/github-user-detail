4a549cb0a17308444c182d615934e02099f56695
Neha Panda <112271756+Neha00011@users.noreply.github.com>
2023-11-06 15:24:38 +0000

    updated functionality

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ submit.addEventListener("click", function (e) {
     .then((data) => {
       //console.log(data);
       //console.log(typeof data);
-      document.querySelector("#my-card").innerHTML = `"User Details"<br>
+      const list=document.getElementById('input-part');
+      list.removeChild(list.firstElementChild);
+      document.getElementById('card').style.background= "purple";
+      document.querySelector("#avt img").setAttribute("src",`${data.avatar_url}`);
+      document.querySelector("#my-card").innerHTML = `
+      "User Details"<br>
       Name=${data.name},<br>
       Public_repos=${data.public_repos},<br>
       Followers=${data.followers},<br>
